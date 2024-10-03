b3bafd76356a714729e1f887f113d194d2f08f33
Eduardo Lucca <lucca.ed@gmail.com>
2024-10-03 21:15:25 +0300

    ex05 and ex06

--- Piscine/C_00/ex06/ft_print_comb2.c
+++ Piscine/C_00/ex06/ft_print_comb2.c
@@ -12,14 +12,52 @@
 
 #include <unistd.h>
 
-void	ft_print_comb2(void)
+void	insert_numb(int nb)
 {
+    char	value;
 
+    value = 0;
+    if (nb < 10)
+    {
+        value = nb + 48;
+        write(1, "0", 1);
+        write(1, &value, 1);
+        return;
+    }
+    value = (nb / 10) + 48;
+    write(1, &value, 1);
+    value = (nb % 10) + 48;
+    write(1, &value, 1);
 }
 
-int	main(int ac, char **av)
+void	ft_print_comb2(void)
 {
+    int a;
+    int b;
 
+    a = 0;
+    while (a <= 98)
+    {
+        b = a + 1;
+        while (b <= 99)
+        {
+            insert_numb(a);
+            write(1, " ", 1);
+            insert_numb(b);
+            if (!(a == 98 && b == 99))
+            {
+                write(1, ",", 1);
+                write(1, " ", 1);
+            }
+            b++;
+        }
+        a++;
+    }
+}
 
+int	main(void)
+{
+    ft_print_comb2();
+    return(0);
 }
 
